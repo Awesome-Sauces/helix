db368dac471a5b75ae64f4ff1babd6dc8abf5a64
Awesome-Sauces <jmorenoalan@gmail.com>
2023-12-12 23:26:42 -0800

    instrutions-b0.0.1

--- core/transaction/struct.go
+++ core/transaction/struct.go
@@ -4,14 +4,5 @@ type Transaction struct {
 	sender   string
 	receiver string
 
-	instructions string
+	instructs string
 }
-
-/*
-
-A transaction is not a set payment, the sender of a transaction
-must provide the subsequent `instructions` for their transaction.
-The sender may choose to include a payment mechanism in the transaction
-instructions or a complex entanglement of funds between multiple accounts.
-
-*/
